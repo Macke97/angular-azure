f541689dd1617fc6d0969289b558f6a5f3b482e3
Marcus Thelin <marcusthelin97@gmail.com>
2018-06-22 15:13:20 +0200

    angular stuff

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
